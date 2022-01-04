8c9bab4965200173a1f549cbebfc16cc8e43160a
Alex Kirk <akirk@users.noreply.github.com>
2022-01-04 20:41:05 +0100

    Add vendor dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a399624bb20f021b0aca48ecc7abb8313e37ec61',
+        'name' => 'alex/friends-parser-fraidyscrape',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alex/friends-parser-fraidyscrape' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a399624bb20f021b0aca48ecc7abb8313e37ec61',
+            'dev_requirement' => false,
+        ),
+        'galbar/jsonpath' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../galbar/jsonpath',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'eab3a7e97d1bcf23b77df87c5ada241789b17c79',
+            'dev_requirement' => false,
+        ),
+        'sabre/uri' => array(
+            'pretty_version' => '2.2.2',
+            'version' => '2.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sabre/uri',
+            'aliases' => array(),
+            'reference' => '7cb0f489578afad5006e85cd60f18ff33f2d440d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
