7c8c92701abdff79035386facab3b25c904df90b
Alex Kirk <akirk@users.noreply.github.com>
2022-04-09 07:30:45 +0200

    Version bump

--- friends-parser-fraidyscrape.php
+++ friends-parser-fraidyscrape.php
@@ -3,7 +3,7 @@
  * Plugin name: Friends Parser Fraidyscrape
  * Plugin author: Alex Kirk
  * Plugin URI: https://github.com/akirk/friends-parsers-fraidyscrape
- * Version: 1.1
+ * Version: 1.2
  *
  * Description: Provides the parsing capabilities of Fraidyscrape (the parser behind Fraidycat).
  *
